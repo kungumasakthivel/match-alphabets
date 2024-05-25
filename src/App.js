201f25998794696f03a89292228b940ca9115a6f
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-05-25 20:41:26 +0530

    error not showing cards

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Card from './Card';
 
 const initialCards = [
-  { id: 1, matched: false, value: 'A' },
-  { id: 2, matched: false, value: 'A' },
-  { id: 3, matched: false, value: 'B' },
-  { id: 4, matched: false, value: 'B' },
-  { id: 5, matched: false, value: 'C' },
-  { id: 6, matched: false, value: 'C' },
-  { id: 7, matched: false, value: 'D' },
-  { id: 8, matched: false, value: 'D' },
-  { id: 9, matched: false, value: 'E' },
-  { id: 10, matched: false, value: 'E' },
-  { id: 11, matched: false, value: 'F' },
-  { id: 12, matched: false, value: 'F' }
+  { id: 1, matched: false, value: 'A', isEven:false },
+  { id: 2, matched: false, value: 'A', isEven:true },
+  { id: 3, matched: false, value: 'B', isEven:false },
+  { id: 4, matched: false, value: 'B', isEven:true },
+  { id: 5, matched: false, value: 'C', isEven:false },
+  { id: 6, matched: false, value: 'C', isEven:true },
+  { id: 7, matched: false, value: 'D', isEven:false },
+  { id: 8, matched: false, value: 'D', isEven:true },
+  { id: 9, matched: false, value: 'E', isEven:false },
+  { id: 10, matched: false, value: 'E', isEven:true },
+  { id: 11, matched: false, value: 'F', isEven:false },
+  { id: 12, matched: false, value: 'F', isEven:true }
 ];
 
 export default function App() {
@@ -76,6 +76,17 @@ export default function App() {
     setCards(shuffleArray([...initialCards]));
   };
 
+  function IsEven(card) {
+    if(card.isEven === false) {
+      <Card key={card.key} card={card} onClick={() => flipCard(card)} />
+    }
+  }
+
+  function NotEven(card) {
+    if(card.isEven === true) {
+      <Card key={card.key} card={card} onClick={() => flipCard(card)} />
+    }
+  }
   return (
     <>
       <div className="scoreboard">
@@ -84,9 +95,20 @@ export default function App() {
         <button onClick={resetGame} >Reset Game</button>
       </div>
       <div className="board">
-        {cards.map(card => (
-          <Card key={card.id} card={card} onClick={() => flipCard(card)} />
-        ))}
+          <div>
+            {
+              cards.map(card => (
+                <IsEven key={card.key} card={card} />
+              ))
+            }
+          </div>
+          <div>
+            {
+              cards.map(card => (
+                <NotEven key={card.key} card={card} />
+              ))
+            }
+          </div>
       </div>
     </>
   );
